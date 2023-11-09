a89a662d649f4475ac789524c5da69780400714d
ManuelBarba06 <113741996+ManuelBarba06@users.noreply.github.com>
2023-11-08 19:12:37 -0600

    174_MABG_FEAT_Add env file and update db file, customer and user

--- db/connection.ts
+++ db/connection.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-
 const db = async() => {
     try{
        
@@ -9,7 +8,7 @@ const db = async() => {
 
         //Connection databse
         await mongoose.connect(
-            'mongodb+srv://admin:root@cluster0.pzvdy2j.mongodb.net/mastergas'
+            `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@${process.env.CLUSTER}.pzvdy2j.mongodb.net/${process.env.DB_NAME}`
         )
 
         console.log('Database online')
